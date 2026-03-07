96bcaf8866054d03f0f5c9ce39529ebd1e8cd1cb
skaletin231 <jacobskaggs03@yahoo.com>
2026-03-07 18:22:20 -0500

    updating main page

--- Javascript/carousel.js
+++ Javascript/carousel.js
@@ -1,23 +1,47 @@
-const slides = document.querySelectorAll(".slide");
-let currentSlide = 0;
+const gameContent = document.querySelectorAll(".game");
+const contentButtons = document.querySelectorAll(".content-button");
+let currentContent = 0;
 
-function OpenSlide(index)
-{
-    slides[currentSlide].classList.remove("active");
-    currentSlide = index;
-    slides[currentSlide].classList.add("active");
-}
+gameContent.forEach(game => {
+    const slides = game.querySelectorAll(".slide");
+    const dots = game.querySelectorAll(".dot");
 
-document.querySelector(".right").onclick = () => {
-    let nextPage = (currentSlide + 1) % slides.length;
-    OpenSlide(nextPage);
-};
+    let currentSlide = 0;
 
-document.querySelector(".left").onclick = () => {
-    let nextPage = currentSlide - 1;
-    if (nextPage < 0)
+    function OpenSlide(index)
     {
-        nextPage = slides.length-1;
+        slides[currentSlide].classList.remove("active");
+        dots[currentSlide].classList.remove("active");
+
+        currentSlide = index;
+        slides[currentSlide].classList.add("active");
+        dots[currentSlide].classList.add("active");
     }
-    OpenSlide(nextPage);
-};
+
+    game.querySelector(".right").onclick = () => {
+        let nextPage = (currentSlide + 1) % slides.length;
+        OpenSlide(nextPage);
+    };
+
+    game.querySelector(".left").onclick = () => {
+        let nextPage = currentSlide - 1;
+        if (nextPage < 0)
+        {
+            nextPage = slides.length-1;
+        }
+        OpenSlide(nextPage);
+    };    
+})
+
+
+contentButtons.forEach((button, index) => {
+    button.addEventListener('click', () => {
+        console.log("clicked");
+        contentButtons[currentContent].classList.remove("active");
+        gameContent[currentContent].classList.remove("active");
+
+        currentContent = index;
+        contentButtons[currentContent].classList.add("active");
+        gameContent[currentContent].classList.add("active");
+    });
+});
